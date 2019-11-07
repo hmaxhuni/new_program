46492b8a4de2abba108bd736e63f304abef466a2
hmaxhuni <hmaxhuni@citycollege.sheffield.eu>
2019-11-07 14:16:01 +0200

    uploaded ForLoopDemo.java file

--- ForLoopDemo.java
+++ ForLoopDemo.java
@@ -3,6 +3,8 @@ public class ForLoopDemo
 	public static void main(String[] args) {
 		for(int i=0; i < 5; i++){
 			System.out.println("git");
+			System.out.println("git_1");
+			System.out.println("git_2");
 		}
 	}
 }
